dafb58cdb1268b6d1b7cc9345f30a1a19a398025
MaianeSamara <maianesamara@gmail.com>
2022-11-02 15:14:54 -0300

    Finalizando customizações da interface

---
+++ todoApp/app/src/main/java/util/ButtonColumnCellRederer.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+/**
+ *
+ * @author maian
+ */
+public class ButtonColumnCellRederer extends DefaultTableCellRenderer{
+    
+    private String buttonType;
+    
+     
+    public ButtonColumnCellRederer(String buttonType) {
+        this.buttonType = buttonType;
+    }
+
+    public String getButtonType() {
+        return buttonType;
+    }
+
+    public void setButtonType(String buttonType) {
+        this.buttonType = buttonType;
+       
+    }
+    
+    public Component getTableCellRendererComponent(JTable table, Object value,
+            boolean isSelected, boolean hasFocus, int row, int column) {
+        
+//      Cells are by default rendered as a JLabel.
+        JLabel label = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        label.setHorizontalAlignment(CENTER);
+        label.setIcon(new javax.swing.ImageIcon(getClass().getResource("/" + buttonType + ".png")));
+         
+         
+         
+//       Return the JLabel which renders the cell.
+         return label;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
